c109c59dd9b85f5915a4aaf9e48c7877f1bb81ec
sgriffin10 <sgriffin4@babson.edu>
2024-04-27 12:46:48 -0400

    remove isLoading set to true

--- lib/flutter_polls.dart
+++ lib/flutter_polls.dart
@@ -304,7 +304,7 @@ class FlutterPolls extends HookWidget {
                                   Text(
                                     totalVotes.value == 0
                                         ? "0 $votesText"
-                                        : '${(pollOption.votes / totalVotes.value * 100).toStringAsFixed(0)}%',
+                                        : '${(pollOption.votes / totalVotes.value * 100).toStringAsFixed(1)}%',
                                     style: votedPercentageTextStyle,
                                   ),
                                 ],
@@ -324,7 +324,7 @@ class FlutterPolls extends HookWidget {
 
                               votedOption.value = pollOption;
 
-                              isLoading.value = true;
+                              // isLoading.value = true;
 
                               bool success = await onVoted(
                                 votedOption.value!,
